99614e95c524746e11fae73345295027ca8d19de
Ruslan-UA <mnemonikus69@gmail.com>
2023-10-11 16:33:18 +0300

    gulp setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,76 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const del = require('del');
-const rename = require('gulp-rename');
-const cleanCSS = require('gulp-clean-css')
-const babel = require('gulp-babel')
-const uglify = require('gulp-uglify')
-const concat = require('gulp-concat')
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const del = require("del");
+const rename = require("gulp-rename");
+const cleanCSS = require("gulp-clean-css");
+const babel = require("gulp-babel");
+const uglify = require("gulp-uglify");
+const concat = require("gulp-concat");
+const sourcemaps = require("gulp-sourcemaps");
+const autoprefixer =require('gulp-autoprefixer')
+const imagemin = require('gulp-imagemin')
 
 const paths = {
-    styles: {
-        src: ['src/styles/**/*.scss', 'src/styles/**/*.sass'], 
-        dest: 'dist/css/',
-    },
-    scripts: {
-        src: 'src/scripts/**/*.js',
-        dest: 'dist/js/',
-    }
-}
+  styles: {
+    src: "src/styles/**/*.scss",
+    dest: "dist/css/",
+  },
+  scripts: {
+    src: "src/scripts/**/*.js",
+    dest: "dist/js/",
+  },
+};
 
 function clean() {
-    return del(['dist'])
-};
+  return del(["dist"]);
+}
 
 function styles() {
-    return(
-    gulp.src(paths.styles.src)
-    .pipe(sass().on('error', sass.logError))
-    .pipe(cleanCSS())
-    .pipe(rename({
-        basename: 'main', 
-        suffix: '.min',
+  return gulp
+    .src(paths.styles.src)
+    .pipe(sourcemaps.init())
+    .pipe(sass().on("error", sass.logError))
+    .pipe(autoprefixer({
+        cascade: false
     }))
-    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(cleanCSS({
+        level: 2,
+    }))
+    .pipe(
+      rename({
+        basename: "main",
+        suffix: ".min",
+      })
     )
-};
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(paths.styles.dest));
+}
 
 function scripts() {
-    return(
-        gulp.src(paths.scripts.src, {
-            sourcemaps: true
-        })
-        .pipe(babel())
-        .pipe(uglify())
-        .pipe(concat('main.min.js'))
-        .pipe(gulp.dest(paths.scripts.dest))
-    )
+  return gulp
+    .src(paths.scripts.src, {
+      sourcemaps: true,
+    })
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+       presets: ['@babel/env']
+    }))
+    .pipe(uglify())
+    .pipe(concat("main.min.js"))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(paths.scripts.dest));
 }
 
 function watch() {
-        gulp.watch(paths.styles.src, styles)
-        gulp.watch(paths.scripts.src, scripts)
-};
+  gulp.watch(paths.styles.src, styles);
+  gulp.watch(paths.scripts.src, scripts);
+}
 
 const build = gulp.series(clean, gulp.parallel(styles, scripts), watch);
 
-exports.clean = clean
-exports.styles = styles
-exports.scripts = scripts
-exports.watch = watch
-exports.build = build
-exports.defoult = build
+exports.clean = clean;
+exports.styles = styles;
+exports.scripts = scripts;
+exports.watch = watch;
+exports.build = build;
+exports.default = build;
---
+++ src/scripts/main.js
@@ -0,0 +1,12 @@
+class people {
+  seyHi() {
+    console.log("hi");
+  }
+}
+
+const one = {
+  gg: "dfdf",
+  id: 23,
+};
+
+console.log(one);
